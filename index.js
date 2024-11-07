6e8dfcf34d420e5af5c49c21605669579657b76c
Henrique Diomedes <j.hyyk77@gmail.com>
2024-11-06 21:30:27 -0300

    up

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-function abrirModal() {
-    // Exibe o modal no centro da tela
-    document.getElementById("modal").style.display = "block";
-}
+function abrirNaMesmaJanela() {  
+    // Redireciona para a nova página (index2.html) na mesma aba  
+    window.location.href = 'index2.html';  
+}  
 
-function desviar(btn) {
-    // Faz o botão "NÃO" desviar do cursor
-    var larguraJanela = window.innerWidth;
-    var alturaJanela = window.innerHeight;
-    var novaPosicaoTop = Math.random() * (alturaJanela - btn.clientHeight);
-    var novaPosicaoLeft = Math.random() * (larguraJanela - btn.clientWidth);
-    btn.style.position = "absolute";
-    btn.style.top = novaPosicaoTop + "px";
-    btn.style.left = novaPosicaoLeft + "px";
-}
-
-function mostrarPopup() {
-    // Exibe um pop-up com o emoji de joinha
-    alert("vamos com tudo!👍");
+function desviar(btn) {  
+    // Faz o botão "NÃO" desviar do cursor  
+    var larguraJanela = window.innerWidth;  
+    var alturaJanela = window.innerHeight;  
+    var novaPosicaoTop = Math.random() * (alturaJanela - btn.clientHeight);  
+    var novaPosicaoLeft = Math.random() * (larguraJanela - btn.clientWidth);  
+    btn.style.position = "absolute";  
+    btn.style.top = novaPosicaoTop + "px";  
+    btn.style.left = novaPosicaoLeft + "px";  
 }

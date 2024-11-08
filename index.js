d043d684cbca686e9dc5ca90a3fcf9696ecdfa47
Henrique Diomedes <j.hyyk77@gmail.com>
2024-11-08 00:07:50 -0300

    up

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Função para redirecionar para outra página  
 function abrirNaMesmaJanela() {  
     window.location.href = 'index2.html';  
-  }  
+}  
   
   // Função para fazer o botão "NÃO" desviar do cursor  
-  function desviar(btn) {  
+function desviar(btn) {  
     var larguraJanela = window.innerWidth;  
     var alturaJanela = window.innerHeight;  
     var novaPosicaoTop = Math.random() * (alturaJanela - btn.clientHeight);  
@@ -12,13 +12,13 @@ function abrirNaMesmaJanela() {
     btn.style.position = "absolute";  
     btn.style.top = novaPosicaoTop + "px";  
     btn.style.left = novaPosicaoLeft + "px";  
-  }  
+}  
   
   // Variável para rastrear qual imagem está em zoom  
-  let currentZoomed = null;  
+let currentZoomed = null;  
   
   // Função para zoomar imagem  
-  function zoomImage(event, box) {  
+function zoomImage(event, box) {  
     event.stopPropagation();  
     if (currentZoomed === box) {  
       box.classList.remove('zoom');  
@@ -30,10 +30,18 @@ function abrirNaMesmaJanela() {
       box.classList.add('zoom');  
       currentZoomed = box;  
     }  
-  }  
+}  
+
+    // Função para remover o zoom ao clicar fora  
+document.addEventListener('click', () => {  
+        if (currentZoomed) {  
+          currentZoomed.classList.remove('zoom');  
+          currentZoomed = null;  
+        }  
+});
   
   // Array de mensagens para os botões  
-  const buttonMessages = [
+const buttonMessages = [
     "Eu escolho a opção de número 0",   
     "Eu escolho a opção de número 1",  
     "Eu escolho a opção de número 2",  
@@ -45,22 +53,14 @@ function abrirNaMesmaJanela() {
     "Eu escolho a opção de número 8",  
     "Eu escolho a opção de número 9",  
     "Eu escolho a opção de número 10",  
-  ];  
+];  
   
   // Função para enviar mensagem no WhatsApp  
-  function sendToWhatsApp(botaoIndex) {  
+function sendToWhatsApp(botaoIndex) {  
     console.log("Índice do botão:", botaoIndex);  
     console.log("Mensagem:", buttonMessages[botaoIndex]);  
     const recipientNumber = "+559181121979";  
     const message = encodeURIComponent(`Olá, ${buttonMessages[botaoIndex]}, que horas você vem me pegar?`);  
     const whatsappUrl = `https://wa.me/${recipientNumber}?text=${message}`;  
     window.open(whatsappUrl, '_blank');  
-  }
-  
-  // Função para remover o zoom ao clicar fora  
-  document.addEventListener('click', () => {  
-    if (currentZoomed) {  
-      currentZoomed.classList.remove('zoom');  
-      currentZoomed = null;  
-    }  
-  });
+}

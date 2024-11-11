b167b2db53f30cc4c64ef3a63f8ad8e6b14a5423
Henrique Diomedes <j.hyyk77@gmail.com>
2024-11-11 17:02:52 -0300

    up

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 function abrirNaMesmaJanela() {  
     window.location.href = 'index2.html';  
 }  
-  
-  // Função para fazer o botão "NÃO" desviar do cursor  
+
+// Função para fazer o botão "NÃO" desviar do cursor  
 function desviar(btn) {  
     var larguraJanela = window.innerWidth;  
     var alturaJanela = window.innerHeight;  
@@ -13,36 +13,48 @@ function desviar(btn) {
     btn.style.top = novaPosicaoTop + "px";  
     btn.style.left = novaPosicaoLeft + "px";  
 }  
-  
-  // Variável para rastrear qual imagem está em zoom  
+
+// Variável para rastrear qual imagem está em zoom  
 let currentZoomed = null;  
-  
-  // Função para zoomar imagem  
+
+// Função para zoomar imagem  
 function zoomImage(event, box) {  
     event.stopPropagation();  
     if (currentZoomed === box) {  
-      box.classList.remove('zoom');  
-      currentZoomed = null;  
+        box.classList.remove('zoom');  
+        currentZoomed = null;  
     } else {  
-      if (currentZoomed) {  
-        currentZoomed.classList.remove('zoom');  
-      }  
-      box.classList.add('zoom');  
-      currentZoomed = box;  
+        if (currentZoomed) {  
+            currentZoomed.classList.remove('zoom');  
+        }  
+        box.classList.add('zoom');  
+        currentZoomed = box;  
     }  
 }  
 
-    // Função para remover o zoom ao clicar fora  
+// Função para remover o zoom ao clicar fora  
 document.addEventListener('click', () => {  
-        if (currentZoomed) {  
-          currentZoomed.classList.remove('zoom');  
-          currentZoomed = null;  
-        }  
-});
-  
-  // Array de mensagens para os botões  
-const buttonMessages = [
-    "Eu escolho a opção de número 0",   
+    if (currentZoomed) {  
+        currentZoomed.classList.remove('zoom');  
+        currentZoomed = null;  
+    }  
+});  
+
+// Função para flipar e zoomar carta  
+function flipAndZoom(event, box) {  
+  card.classList.add('flip');  
+  card.classList.add('zoom');  
+}  
+
+// Função para resetar flip e zoom da carta  
+function resetFlipAndZoom(event, box) {  
+  card.classList.remove('flip');  
+  card.classList.remove('zoom');  
+}
+
+// Array de mensagens para os botões  
+const buttonMessages = [  
+    "Eu escolho a opção de número 0",  
     "Eu escolho a opção de número 1",  
     "Eu escolho a opção de número 2",  
     "Eu escolho a opção de número 3",  
@@ -54,8 +66,8 @@ const buttonMessages = [
     "Eu escolho a opção de número 9",  
     "Eu escolho a opção de número 10",  
 ];  
-  
-  // Função para enviar mensagem no WhatsApp  
+
+// Função para enviar mensagem no WhatsApp  
 function sendToWhatsApp(botaoIndex) {  
     console.log("Índice do botão:", botaoIndex);  
     console.log("Mensagem:", buttonMessages[botaoIndex]);  
@@ -63,15 +75,5 @@ function sendToWhatsApp(botaoIndex) {
     const message = encodeURIComponent(`Olá, ${buttonMessages[botaoIndex]}, que horas você vem me pegar?`);  
     const whatsappUrl = `https://wa.me/${recipientNumber}?text=${message}`;  
     window.open(whatsappUrl, '_blank');  
-}
-// Função para flipar e zoomar carta  
-function flipAndZoom(event, card) {  
-  card.classList.add('flip');  
-  card.classList.add('zoom');  
 }  
 
-// Função para resetar flip e zoom da carta  
-function resetFlipAndZoom(event, card) {  
-  card.classList.remove('flip');  
-  card.classList.remove('zoom');  
-}

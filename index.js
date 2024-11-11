8fc5a4c78218fdab05ec8a50e8483f6ae59d1c0a
Henrique Diomedes <j.hyyk77@gmail.com>
2024-11-11 17:59:32 -0300

    up

--- index.js
+++ index.js
@@ -1,79 +1,87 @@
 // Função para redirecionar para outra página  
 function abrirNaMesmaJanela() {  
-    window.location.href = 'index2.html';  
+  window.location.href = 'index2.html';  
 }  
 
 // Função para fazer o botão "NÃO" desviar do cursor  
 function desviar(btn) {  
-    var larguraJanela = window.innerWidth;  
-    var alturaJanela = window.innerHeight;  
-    var novaPosicaoTop = Math.random() * (alturaJanela - btn.clientHeight);  
-    var novaPosicaoLeft = Math.random() * (larguraJanela - btn.clientWidth);  
-    btn.style.position = "absolute";  
-    btn.style.top = novaPosicaoTop + "px";  
-    btn.style.left = novaPosicaoLeft + "px";  
+  var larguraJanela = window.innerWidth;  
+  var alturaJanela = window.innerHeight;  
+  var novaPosicaoTop = Math.random() * (alturaJanela - btn.clientHeight);  
+  var novaPosicaoLeft = Math.random() * (larguraJanela - btn.clientWidth);  
+  btn.style.position = "absolute";  
+  btn.style.top = novaPosicaoTop + "px";  
+  btn.style.left = novaPosicaoLeft + "px";  
 }  
 
 // Variável para rastrear qual imagem está em zoom  
 let currentZoomed = null;  
 
+// Variável para rastrear qual imagem está em rotação  
+let currentRotating = null;  
+
 // Função para zoomar imagem  
 function zoomImage(event, box) {  
-    event.stopPropagation();  
-    if (currentZoomed === box) {  
-        box.classList.remove('zoom');  
-        currentZoomed = null;  
-    } else {  
-        if (currentZoomed) {  
-            currentZoomed.classList.remove('zoom');  
-        }  
-        box.classList.add('zoom');  
-        currentZoomed = box;  
-    }  
+  event.stopPropagation();  
+  if (currentZoomed === box) {  
+      box.classList.remove('zoom');  
+      currentZoomed = null;  
+  } else {  
+      if (currentZoomed) {  
+          currentZoomed.classList.remove('zoom');  
+          currentZoomed.classList.remove('rotate');  
+          currentRotating = null;  
+      }  
+      box.classList.add('zoom');  
+      currentZoomed = box;  
+      setTimeout(() => {  
+          if (currentZoomed === box) {  
+              rotateImage(box);  
+          }  
+      }, 500);  
+  }  
 }  
 
-// Função para remover o zoom ao clicar fora  
+// Função para rotacionar imagem  
+function rotateImage(box) {  
+  box.classList.add('rotate');  
+  currentRotating = box;  
+}  
+
+// Função para remover o zoom e rotação ao clicar fora  
 document.addEventListener('click', () => {  
-    if (currentZoomed) {  
-        currentZoomed.classList.remove('zoom');  
-        currentZoomed = null;  
-    }  
+  if (currentZoomed) {  
+      currentZoomed.classList.remove('zoom');  
+      currentZoomed.classList.remove('rotate');  
+      currentZoomed = null;  
+      currentRotating = null;  
+  }  
 });  
 
-// Função para flipar e zoomar carta  
-function flipAndZoom(event, box) {  
-  card.classList.add('flip');  
-  card.classList.add('zoom');  
-}  
-
-// Função para resetar flip e zoom da carta  
-function resetFlipAndZoom(event, box) {  
-  card.classList.remove('flip');  
-  card.classList.remove('zoom');  
-}
+ 
 
 // Array de mensagens para os botões  
 const buttonMessages = [  
-    "Eu escolho a opção de número 0",  
-    "Eu escolho a opção de número 1",  
-    "Eu escolho a opção de número 2",  
-    "Eu escolho a opção de número 3",  
-    "Eu escolho a opção de número 4",  
-    "Eu escolho a opção de número 5",  
-    "Eu escolho a opção de número 6",  
-    "Eu escolho a opção de número 7",  
-    "Eu escolho a opção de número 8",  
-    "Eu escolho a opção de número 9",  
-    "Eu escolho a opção de número 10",  
+  "Eu escolho a opção de número 0",  
+  "Eu escolho a opção de número 1",  
+  "Eu escolho a opção de número 2",  
+  "Eu escolho a opção de número 3",  
+  "Eu escolho a opção de número 4",  
+  "Eu escolho a opção de número 5",  
+  "Eu escolho a opção de número 6",  
+  "Eu escolho a opção de número 7",  
+  "Eu escolho a opção de número 8",  
+  "Eu escolho a opção de número 9",  
+  "Eu escolho a opção de número 10",  
 ];  
 
 // Função para enviar mensagem no WhatsApp  
 function sendToWhatsApp(botaoIndex) {  
-    console.log("Índice do botão:", botaoIndex);  
-    console.log("Mensagem:", buttonMessages[botaoIndex]);  
-    const recipientNumber = "+559181121979";  
-    const message = encodeURIComponent(`Olá, ${buttonMessages[botaoIndex]}, que horas você vem me pegar?`);  
-    const whatsappUrl = `https://wa.me/${recipientNumber}?text=${message}`;  
-    window.open(whatsappUrl, '_blank');  
-}  
+  console.log("Índice do botão:", botaoIndex);  
+  console.log("Mensagem:", buttonMessages[botaoIndex]);  
+  const recipientNumber = "+559181121979";  
+  const message = encodeURIComponent(`Olá, ${buttonMessages[botaoIndex]}, que horas você vem me pegar?`);  
+  const whatsappUrl = `https://wa.me/${recipientNumber}?text=${message}`;  
+  window.open(whatsappUrl, '_blank');  
+}
 
